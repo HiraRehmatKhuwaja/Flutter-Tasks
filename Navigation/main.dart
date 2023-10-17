af496f706ae8ef553ed9f359fb11629adc2b3558
Hira Rehmat <94304187+HiraRehmatKhuwaja@users.noreply.github.com>
2023-10-17 14:59:26 +0500

    Update main.dart

--- Navigation/main.dart
+++ Navigation/main.dart
@@ -1,1 +1,29 @@
 
+import 'package:flutter/material.dart';
+import 'HomeScreen.dart';
+import 'DetailScreen.dart';
+
+void main()
+{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp
+     (
+      debugShowCheckedModeBanner: false,
+      title: 'Simple App',
+      initialRoute: '/',
+      routes:
+      {
+        '/': (context) => const HomeScreen(title: "Welcome to Home Screen"),
+        '/detail': (context) => const DetailScreen(title: "Welcome to Detail Screen"),
+      },
+    );
+  }
+}
